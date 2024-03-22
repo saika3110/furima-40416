b2b9891d08d9222a764559a00b601c6a372b72ea
saika3110 <143299649+saika3110@users.noreply.github.com>
2024-03-22 15:27:46 +0900

    ユーザー管理機能（userカラムの追加）

--- db/migrate/20240321215617_devise_create_users.rb
+++ db/migrate/20240321215617_devise_create_users.rb
@@ -3,10 +3,16 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :nickname,           null: false
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name_kana,      null: false
+      t.string :first_name_kana,     null: false
+      t.date   :birth_date,          null: false
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,12 @@
 
 ActiveRecord::Schema[7.0].define(version: 2024_03_21_215617) do
   create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birth_date", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
