6ba0d732d28fb359099a84ab8eb79515584af54b
saika3110 <143299649+saika3110@users.noreply.github.com>
2024-03-27 17:19:49 +0900

    articles_controller.rbの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/new'
-  devise_for :users
-  root to: 'items#index'
-  resources :articles
 
+  devise_for :users
+
+  root to: 'items#index'
+
+  resources :items, only: [:index, :new]
 
 end
