395582bc86216a1842a49972eb4ac0bda14d9e90
saika3110 <143299649+saika3110@users.noreply.github.com>
2024-03-29 15:41:09 +0900

    商品入力フォーム・エラーメッセ表示・確認済み

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,10 +11,12 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_day
   
   #空の投稿を保存できないようにする
-  validates :image, :item_name, :item_info, :price, :category_id, :prefecture_id,  presence: true
+  validates :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :image, :item_name, :item_info, :price, presence: true
+
   #ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, presence: true
-  
+  validates :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
+
+
   validates :price, numericality: { only_integer: true , greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,11 +3,11 @@ FactoryBot.define do
     item_name { "MyString" }
     item_info { "MyText" }
     price { 301 }
-    category_id { 1 }
-    condition_id { 1 }
-    shipping_cost_id { 1 }
-    prefecture_id { 1 }
-    shipping_day_id { 1 }
+    category_id { 2 }
+    condition_id { 2 }
+    shipping_cost_id { 2 }
+    prefecture_id { 2 }
+    shipping_day_id { 2 }
     image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test_image.jpg')) }
     association :user 
   end
