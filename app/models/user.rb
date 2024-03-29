44bcb94de58b78475b9c33540e0a67e3b5f73da3
saika3110 <143299649+saika3110@users.noreply.github.com>
2024-03-29 19:58:33 +0900

    deployエラー一部修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ validates :nickname, presence: true
 validates :last_name_kana, :first_name_kana, format: { with: VALID_KANA_REGEX, message: '全角カタカナ使用してください' }
 validates :last_name_kana, :first_name_kana, format: { with: VALID_KANA_REGEX, message: '全角ひらがなのみで入力して下さい' }      
 validates :last_name_kana, :first_name_kana, format: { with: VALID_KANA_REGEX, message: '半角英字を使用してください' } 
-validates :last_name, :first_name, format: { with: VALID_NAME_REGEX, message: '全角（漢字・ひらがな・カタカナ）を使用してください' }
+
 validates :last_name, :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: '全角（漢字・ひらがな・カタカナ）を使用してください' }
 validates :birth_date, presence: true
 validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Input half-width characters and numbers.' }
